90ef2c37407b65b3ac6de0ee9946ea7f51224683
jefah2109 <jefah2109@gmail.com>
2018-05-03 16:55:48 +0100

    restricting access to view

--- tictactoe/tictactoe/views.py
+++ tictactoe/tictactoe/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 
 def welcome(request):
-    if request.is_authenticated:
+    if request.user.is_authenticated:
         return redirect('player_home')
     else:
         return render(request, 'tictactoe/welcome.html')
